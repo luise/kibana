6dd82b606a2a634c2b093c51a02f1cffe15fc262
Kevin Lin <kevin@quilt.io>
2017-08-14 16:42:59 -0700

    blueprint: Use new Quilt API
    The Quilt API now deploys containers directly.

--- kibana.js
+++ kibana.js
@@ -1,18 +1,18 @@
-const { Service, Container, publicInternet } = require('@quilt/quilt');
+const { Container, publicInternet } = require('@quilt/quilt');
 
 function Kibana(es) {
-  this.service = new Service('kibana', [
-    new Container('kibana:4',
-      [
-        '--port', this.port.toString(),
-        '--elasticsearch', es.uri(),
-      ])]);
-  es.addClient(this.service);
-  this.service.allowFrom(publicInternet, this.port);
+  this.container = new Container('kibana', 'kibana:4', {
+    command: [
+      '--port', this.port.toString(),
+      '--elasticsearch', es.uri(),
+    ],
+  });
+  es.addClient(this.container);
+  this.container.allowFrom(publicInternet, this.port);
 }
 
 Kibana.prototype.deploy = function deploy(depl) {
-  depl.deploy(this.service);
+  depl.deploy(this.container);
 };
 
 Kibana.prototype.port = 5601;
