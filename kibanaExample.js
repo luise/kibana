d11441853db541707b471c7f91bf5e076fdcff34
Luise Valentin <luise@kelda.io>
2017-10-27 14:07:09 -0700

    blueprint: Use Infrastructure constructor
    This commit replaces the deprecated `createDeployment` function with the
    new `Infrastructure` constructor.

--- kibanaExample.js
+++ kibanaExample.js
@@ -1,15 +1,15 @@
-const { createDeployment, Machine } = require('kelda');
+const { Infrastructure, Machine } = require('kelda');
 const Elasticsearch = require('@kelda/elasticsearch').Elasticsearch;
 const Kibana = require('./kibana.js').Kibana;
 
 const clusterSize = 2;
 
-const deployment = createDeployment({});
 const baseMachine = new Machine({ provider: 'Amazon' });
-deployment.deploy(baseMachine.asMaster());
-deployment.deploy(baseMachine.asWorker().replicate(clusterSize));
+const infra = new Infrastructure(
+  baseMachine,
+  baseMachine.replicate(clusterSize));
 
 const es = new Elasticsearch(clusterSize);
-es.deploy(deployment);
+es.deploy(infra);
 const kib = new Kibana(es);
-kib.deploy(deployment);
+kib.deploy(infra);
