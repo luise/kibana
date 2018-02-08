c2bc0618d3c09e89133b289686edc750789b6c5d
Kevin Lin <kevin@kelda.io>
2018-02-08 09:38:07 -0800

    example: Specify the machine size
    Machines that are too small take a really long time to build the Kibana
    image.

--- kibanaExample.js
+++ kibanaExample.js
@@ -1,7 +1,10 @@
 const kelda = require('kelda');
 const Kibana = require('./kibana.js').Kibana;
 
-const baseMachine = new kelda.Machine({ provider: 'Amazon' });
+// Installing plugins can take a while, so using a larger machine greatly speeds
+// up the boot time. Installing a plugin on m3.large takes about 5 minutes,
+// while m3.medium takes about 10 minutes.
+const baseMachine = new kelda.Machine({ provider: 'Amazon', size: 'm3.large' });
 const infra = new kelda.Infrastructure(baseMachine, baseMachine);
 
 const elasticsearchURL = '<ELASTICSEARCH URL>';
